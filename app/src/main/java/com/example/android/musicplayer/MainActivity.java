38f51688bcff2b418fb9d496a3d4182bac0604d7
Diederik de Neef <diederik.deneef@gmail.com>
2020-07-03 14:05:46 +0200

    feat: Add RecyclerView to Que on Now Playing
    Add a minimum working version of a RecyclerView that forms the
    basis of the Que on the Now Playing activity. An onClickListener
    is included and currently shows a Toast message.

--- app/src/main/java/com/example/android/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/android/musicplayer/MainActivity.java
@@ -1,14 +1,71 @@
 package com.example.android.musicplayer;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements SongAdapter.ItemClickListener {
+
+    SongAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // data to populate the RecyclerView with
+        ArrayList<String> songTitles = new ArrayList<>();
+        songTitles.add("song a");
+        songTitles.add("song b");
+        songTitles.add("song c");
+        songTitles.add("song d");
+        songTitles.add("song e");
+        songTitles.add("song f");
+        songTitles.add("song g");
+        songTitles.add("song h");
+        songTitles.add("song i");
+        songTitles.add("song j");
+        songTitles.add("song k");
+        songTitles.add("song l");
+        songTitles.add("song m");
+        songTitles.add("song n");
+        songTitles.add("song o");
+        songTitles.add("song p");
+        songTitles.add("song q");
+        songTitles.add("song r");
+        songTitles.add("song s");
+        songTitles.add("song t");
+        songTitles.add("song u");
+        songTitles.add("song v");
+        songTitles.add("song w");
+        songTitles.add("song x");
+        songTitles.add("song y");
+        songTitles.add("song z");
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.que_now_playing);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new SongAdapter(this, songTitles);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(), layoutManager.getOrientation());
+        recyclerView.addItemDecoration(dividerItemDecoration);
     }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
+    }
+
+
 }
