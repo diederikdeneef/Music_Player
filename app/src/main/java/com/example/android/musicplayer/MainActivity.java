dc6a596ab0455fa54a71ded3543455861a56702e
Diederik de Neef <diederik.deneef@gmail.com>
2020-07-07 19:54:55 +0200

    docs: Add comments to the code

--- app/src/main/java/com/example/android/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/android/musicplayer/MainActivity.java
@@ -13,11 +13,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+
+        // Generate sample data to populate the ListView
         ArrayList<SongDetails> songDetails = new ArrayList<SongDetails>();
-        for (int i = 1; i <= 30; i++) {
+        for (int i = 1; i <= 50; i++) {
             songDetails.add(new SongDetails("Song " + i, "Artist " + i));
         }
+
+        // set the adapter
         SongAdapter adapter = new SongAdapter(this, songDetails);
         ListView listView = (ListView) findViewById(R.id.music_library);
         listView.setAdapter(adapter);
--- app/src/main/java/com/example/android/musicplayer/SongAdapter.java
+++ app/src/main/java/com/example/android/musicplayer/SongAdapter.java
@@ -14,6 +14,11 @@ import java.util.ArrayList;
 
 public class SongAdapter extends ArrayAdapter<SongDetails> {
 
+    /**
+     * Constructor
+     * @param context: the current context to inflate the layout file
+     * @param songDetails: an ArrayList with song details
+     */
     public SongAdapter(Activity context, ArrayList<SongDetails> songDetails) {
         super(context, 0, songDetails);
     }
@@ -21,19 +26,24 @@ public class SongAdapter extends ArrayAdapter<SongDetails> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        // check if the existing view is being reused, if not inflate the view
         View listItemView = convertView;
         if(listItemView == null) {
             listItemView = LayoutInflater.from(getContext()).inflate(R.layout.song_item, parent, false);
         }
 
+        // get the songDetails object at this position in the list
         SongDetails currentSong = getItem(position);
 
+        // Find the song title textView in song_item.xml layout and set the song title
         TextView songTitleTextView = (TextView) listItemView.findViewById(R.id.song_title_textView);
         songTitleTextView.setText(currentSong.getSongTitle());
 
+        // Find the artist name textView in song_item.xml layout and set the artist name
         TextView artistNameTextView = (TextView) listItemView.findViewById(R.id.artist_name_textView);
         artistNameTextView.setText(currentSong.getArtistName());
 
+        // return the listItem so it can be shown in the ListView
         return listItemView;
     }
 }
