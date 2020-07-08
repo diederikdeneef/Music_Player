685a6a4a897371c9ebbb41205d73a7a8327d2b87
Diederik de Neef <diederik.deneef@gmail.com>
2020-07-08 11:11:01 +0200

    feat: Add Now Playing Activity
    When you click on a song in the Music Library, the Now Playing
    will start.

--- app/src/main/java/com/example/android/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/android/musicplayer/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.android.musicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Generate sample data to populate the ListView
-        ArrayList<SongDetails> songDetails = new ArrayList<SongDetails>();
+        final ArrayList<SongDetails> songDetails = new ArrayList<SongDetails>();
         for (int i = 1; i <= 50; i++) {
             songDetails.add(new SongDetails("Song " + i, "Artist " + i));
         }
@@ -27,5 +32,16 @@ public class MainActivity extends AppCompatActivity {
         SongAdapter adapter = new SongAdapter(this, songDetails);
         ListView listView = (ListView) findViewById(R.id.music_library);
         listView.setAdapter(adapter);
+
+        // set an item click listener
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //Log.d("TAG", "onItemClick: song title " + songDetails.get(i));
+                //Toast.makeText(MainActivity.this, "You clicked on: " + songDetails.get(i), Toast.LENGTH_SHORT).show();
+                Intent nowPlayingIntent = new Intent(MainActivity.this, NowPlaying.class );
+                startActivity(nowPlayingIntent);
+            }
+        });
     }
 }
